eb6939b42c607643efbaafd5f608d6553b836dac
Kristina T <kikitiki1989@gmail.com>
2024-02-21 17:25:44 +0100

    ading packages and home page

--- src/main/java/com/bookStore/BookStoreApplication.java
+++ src/main/java/com/bookStore/BookStoreApplication.java
@@ -2,8 +2,10 @@ package com.bookStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+
 public class BookStoreApplication {
 
 	public static void main(String[] args) {
