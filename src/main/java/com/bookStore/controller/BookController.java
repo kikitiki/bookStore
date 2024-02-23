e036efe06504a74ea1651ccd9d0839be417d4613
Kristina T <kikitiki1989@gmail.com>
2024-02-23 18:18:38 +0100

    :adding method for creating new book and method forgettin all book

--- src/main/java/com/bookStore/controller/BookController.java
+++ src/main/java/com/bookStore/controller/BookController.java
@@ -1,10 +1,18 @@
 package com.bookStore.controller;
 
+import com.bookStore.entity.Book;
+import com.bookStore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class BookController {
+    @Autowired
+    private BookService bookService;
     @GetMapping("")
     public String home(){
         return "home";
@@ -14,7 +22,17 @@ public class BookController {
         return "bookRegister";
     }
     @GetMapping("/avaliable_books")
-    public String getAllBook(){
-        return  "bookList";
+    public ModelAndView getAllBook(){
+        List<Book> list = bookService.getAllBooks();
+//        ModelAndView modelAndView =new ModelAndView();
+//        modelAndView.setViewName("Book list");
+//        modelAndView.addObject("book",list);
+        return  new ModelAndView("bookList","book",list);
+    }
+
+    @PostMapping("/save")
+    public String addBook(@ModelAttribute Book book){
+            bookService.save(book);
+            return "redirect:/avaliable_books";
     }
 }
